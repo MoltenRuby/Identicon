f7f451b4303600dd3054b044aa5274118b81c946
Alexandre Morris <alexandre.morris.1@gmail.com>
2019-12-12 21:45:22 -0500

    up to video 41

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,7 +12,24 @@ defmodule Identicon do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
   end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _] = hex_list
+    [r, g, b]
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+
+  end
+
 end
